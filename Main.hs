04f3df71e42a504d66695c8f872dc8251223ad26
Chris Wendt <chrismwendt@gmail.com>
2014-04-11 00:37:59 -0400

    Implement binary background subtraction

--- Main.hs
+++ Main.hs
@@ -10,6 +10,7 @@ import qualified Data.Set as S
 import Numeric.Qhull
 import qualified Data.Vector as V
 import qualified Utility as U
+import qualified Data.Glome.Vec as V3
 
 main = do
     initGUI
@@ -49,29 +50,42 @@ mainWindow continue file = do
                     cols <- readIORef colsRef
                     let rgbaToColorList (r, g, b, a) = map (\c -> fromIntegral c / 255) [r, g, b]
                     let points = V.fromList $ map (V.fromList . rgbaToColorList) $ S.toList cols
-                    return $ (points, qhull' points)
+                    return $ (V.map (\v -> V3.Vec (v V.! 0) (v V.! 1) (v V.! 2)) points, qhull' points)
                 (fgPoints, fgm) <- colsToHull fgs
                 case fgm of
                     Left fgHull -> do
                         (bgPoints, bgm) <- colsToHull bgs
                         case bgm of
                             Left bgHull -> do
-                                subtractBG pb (toTriangles fgPoints fgHull) (toTriangles bgPoints bgHull) >>= continue
+                                subtractBG pb (toTriangles fgPoints (map V.convert fgHull)) (toTriangles bgPoints (map V.convert bgHull)) >>= continue
                             Right e -> putStrLn $ errorMessage e
                     Right e -> putStrLn $ errorMessage e
 
             _ -> liftIO $ print "Only f, b, and c keys are supported."
         return False
 
+subtractBG :: Pixbuf -> [(V3.Vec, V3.Vec, V3.Vec)] -> [(V3.Vec, V3.Vec, V3.Vec)] -> IO Pixbuf
 subtractBG pb fgHull bgHull = do
-    U.pixbufMap pb (\(r, g, b, a) -> let (r', g', b') = modifyColor (r, g, b); a' = modifyAlpha (r, g, b) in (r', g', b', a'))
+    U.pixbufMap pb f
     return pb
+    where
+    f (r, g, b, a) = let
+        (r', g', b') = modifyColor (r, g, b)
+        a' = modifyAlpha (fromIntegral r / 255, fromIntegral g / 255, fromIntegral b / 255) fgHull bgHull
+        in (r', g', b', a')
 
-toTriangles points hull = map (\v -> (points V.! (v V.! 0), points V.! (v V.! 1), points V.! (v V.! 2))) $ map V.convert $ hull
+toTriangles :: V.Vector V3.Vec -> [V.Vector Int] -> [(V3.Vec, V3.Vec, V3.Vec)]
+toTriangles points hull = map (\t -> (points V.! (t V.! 0), points V.! (t V.! 1), points V.! (t V.! 2))) hull
 
-modifyColor (r, g, b) = (r `div` 2, g, b)
+front p (a, b, c) = (p `V3.vsub` a) `V3.vdot` normal > 0
+    where
+    e1 = b `V3.vsub` a
+    e2 = c `V3.vsub` b
+    normal = e1 `V3.vcross` e2
 
-modifyAlpha (r, g, b) = 255
+modifyColor (r, g, b) = (r, g, b)
+
+modifyAlpha (r, g, b) fgHull bgHull = if all (front (V3.Vec r g b)) fgHull then 255 else 0
 
 sample setRef pb pw ph x y
     | x < 0 || y < 0 || x >= pw || y >= ph = return ()
